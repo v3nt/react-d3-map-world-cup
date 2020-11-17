b664ecf37188922713cf12e91451dc125ad5e1d5
v3nt <dan@jynk.net>
2020-11-17 09:05:13 +0000

    save arezzo files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
 import React from "react";
 import WorldMap from "./WorldMap";
 
-class App extends React.Component {}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        geoData: null,
+        cupData: null,
+      },
+    };
+  }
+
+  componentDidMout() {
+    Promise.all([
+      fetch(
+        "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_countries.json"
+      ),
+      fetch(
+        "https://raw.githubusercontent.com/ahebwa49/geo_mapping/master/src/world_cup_geo.json"
+      ),
+    ])
+      .then((responses) => Promise.all(responses.map((resp) => resp.json())))
+      .then(([geoData, cupData]) => {
+        this.setState({
+          data: {
+            geoData: geoData,
+            cupData: cupData,
+          },
+        });
+      });
+  }
+
+  render() {
+    const { data } = this.state;
+    return <div>{data.geoData && <WorldMap data={data} />}</div>;
+  }
+}
 
 export default App;
