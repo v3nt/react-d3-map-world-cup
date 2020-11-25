1e86b62508fa225e0d7f5fb0e3231cae4192693e
v3nt <dan@jynk.net>
2020-11-25 07:59:00 +0000

    chart-basic-bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { useState, useEffect } from "react";
 import WorldMap from "./WorldMap";
+import BarChart from "./D3BarChart";
+
+const datas = [
+  [10, 30, 40, 20],
+  [10, 40, 30, 20, 50, 10],
+  [60, 30, 40, 20, 30],
+];
+var i = 0;
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    changeData();
+  }, []);
+
+  const changeData = () => {
+    setData(datas[i++]);
+    if (i === datas.length) i = 0;
+  };
+
   return (
     <div>
+      <h2>Graphs with React</h2>
+      <button onClick={changeData}>Change Data</button>
+      <BarChart width={600} height={400} data={data} />
+
       <WorldMap />
     </div>
   );
